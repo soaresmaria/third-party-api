728e3172fc93f8a59e79e324c0be751062f1a463
Mona Lisa <soaresmaria181@yahoo.com>
2021-07-25 00:01:48 -0400

    enter event function enabled

--- script.js
+++ script.js
@@ -2,14 +2,33 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
     //display current day & time.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    
-    
-
-
     function hourTracker() {
         //get current number of hours.
         var currentHour = moment().hour();
 
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(blockHour, currentHour)
+
+            //check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
     }
+    hourTracker();
 })
 
