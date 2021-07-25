18c2757fe64e3b22d538dda9f230dc2f92398e13
Mona Lisa <soaresmaria181@yahoo.com>
2021-07-25 02:28:37 -0400

    saved time block in the local storage

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
     //display current day & time.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
+     //Assign saveBtn click listener
+     $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+
+     //saved data from LocalStorage - for each hour created.
+     $("#hour9 .description").val(localStorage.getItem("hour9"));
+     $("#hour10 .description").val(localStorage.getItem("hour10"));
+     $("#hour11 .description").val(localStorage.getItem("hour11"));
+     $("#hour12 .description").val(localStorage.getItem("hour12"));
+     $("#hour13 .description").val(localStorage.getItem("hour13"));
+     $("#hour14 .description").val(localStorage.getItem("hour14"));
+     $("#hour15 .description").val(localStorage.getItem("hour15"));
+     $("#hour16 .description").val(localStorage.getItem("hour16"));
+     $("#hour17 .description").val(localStorage.getItem("hour17"));
+ 
     function hourTracker() {
         //get current number of hours.
         var currentHour = moment().hour();
